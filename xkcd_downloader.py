293422a69201461f28cf3b4803d648fd57d71296
gamseb <gamseb@localhost.localdomain>
2018-10-04 16:08:48 +0200

    Added the try-except clause for better reliability

--- xkcd_downloader.py
+++ xkcd_downloader.py
@@ -16,20 +16,23 @@ while not url.endswith("#"):
 
     # Find the URL of the comic image
     comicElem = soup.select("#comic img")
-    if comicElem == []:
-        print("Could not find the image.")
-    else:
-        comicUrl = "http:" + comicElem[0].get("src")
-        # Download the image
-        print("Downloading image {}...".format(comicUrl))
-        res = requests.get(comicUrl)
-        res.raise_for_status()
+    try:
+        if comicElem == []:
+            print("Could not find the image.")
+        else:
+            comicUrl = "http:" + comicElem[0].get("src")
+            # Download the image
+            print("Downloading image {}...".format(comicUrl))
+            res = requests.get(comicUrl)
+            res.raise_for_status()
 
-        # Save the image to ./xkcd.
-        imageFile = open(os.path.join("xkcd", os.path.basename(comicUrl)), "wb")
-        for chunk in res.iter_content(100000):
-            imageFile.write(chunk)
-        imageFile.close()
+            # Save the image to ./xkcd.
+            imageFile = open(os.path.join("xkcd", os.path.basename(comicUrl)), "wb")
+            for chunk in res.iter_content(100000):
+                imageFile.write(chunk)
+            imageFile.close()
+    except:
+        pass
     # Get the previous button's URL
     prevLink = soup.select('a[rel="prev"]')[0]
     url = "http://xkcd.com" + prevLink.get("href")
